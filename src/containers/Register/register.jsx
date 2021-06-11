0c82a400d35c901980e14bfe97a5407d96f1b030
Goodbyez <miradadesigual@gmail.com>
2021-06-11 17:56:07 +0200

    Register newuser  endpoint working

--- src/containers/Register/register.jsx
+++ src/containers/Register/register.jsx
@@ -10,11 +10,8 @@ const Register = () => {
     let history = useHistory();
 
     // Hooks
-    const [credentials, setCredentials] = useState({name:'',nick:'',email:'',password:'',bithdate:'',country:'',city:'',isAdmin:'',isActive:''});
-    const [errors, setErrors] = useState({
-        eName: '',
-        eEmail: ''
-    });
+    const [credentials, setCredentials] = useState({name:'',nick:'',email:'',password:'',birthdate:'',country:'',city:'',isAdmin:'false',isActive:'false'});
+    const [errors, setErrors] = useState({eName: '',eNick: '',eEmail: '',ePassword:'',eBirthdate:'',eCountry:'',eCity:''});
 
     const [msgError, setMensajeError] = useState('');
 
@@ -27,7 +24,8 @@ const Register = () => {
     const checkError = (arg) => {
         switch (arg){
             case 'name':
-                console.log("aaaaaaaaaaaaaaaa");
+                console.log(credentials)
+                console.log(credentials.name);
                 if(credentials.name.length < 4){
                     console.log("aaaaaaaaaaaaa 22222");
                     setErrors({...errors, eName: 'El nombre debe de tener 4 caracteres'});
@@ -58,61 +56,28 @@ const Register = () => {
             isAdmin: credentials.isAdmin,
             isActive: credentials.isActive
         }
-        console.log('Este es el email, ', body.email);
-        console.log('Este es el password, ', body.password);
-        console.log('Hemos cruzado la frontera', body);
+        console.log('Este es el body que mandamos: ', body);
         
-        // SWITCH ROLES
-        switch (body.options){
 
-            case 'user':
-                let loguser = await axios.post('http://localhost:3005/user/login', body);
-                console.log(loguser.data);
-                
-                if (loguser){          
-                    localStorage.setItem("token", loguser.data.token);
-                    localStorage.setItem("user", JSON.stringify(loguser.data.user));
 
-                    setTimeout (()=>{
-                        history.push("/loguser")
-                    }, 500)
-                }
-                break;
+        axios
+            .post('http://localhost:3005/user/newuser', body)
+            .then((res)=>{})
+            .catch((error)=>{
+                console.log(error);
+            });
+        
+        
+        // if (newOne){          
+        //     localStorage.setItem("token", newOne.data.token);
+        //     localStorage.setItem("user", JSON.stringify(newOne.data.user)    
+        //     setTimeout (()=>{
+        //         history.push("/loguser")
+        //     }, 500)
+        // }
 
-            case 'coach':
-                let logcoach = await axios.post('http://localhost:3005/coach/login', body);
-                console.log(logcoach.data);
 
-                if (logcoach){
-                    localStorage.setItem("token", logcoach.data.token);
-                    localStorage.setItem("user", JSON.stringify(logcoach.data.coach));
-
-                    setTimeout (()=>{
-                        history.push("/logcoach")
-                    }, 500)
-                }
-                break;
-
-            case 'admin':
-
-                let logadmin = await axios.post('http://localhost:3005/user/login', body);
-
-                console.log(logadmin.data.user.isAdmin)
-                
-                if (logadmin.data.user.isAdmin === true){
-                    localStorage.setItem("token", logadmin.data.token);
-                    localStorage.setItem("user", JSON.stringify(logadmin.data.user));
-
-                    setTimeout (()=>{
-                        history.push("/logadmin")
-                    }, 500)
-                }
-
-                // PARA VERIFICARLO HABRÍA QUE INSTALAR JSONWEBTOKEN, o eso o hacemos una tabla de administradores separada de los users
-
-                break;
-
-        }
+   
     }
     
 
@@ -124,20 +89,27 @@ const Register = () => {
             {/* <pre>{JSON.stringify(credentials,null,2)}</pre> */}
             <div className="boxLogin bgGreen">
                 
-                <label className="labelsLogin" for="userName">NAME</label>
-                <input require="true" className="inputsLogin" type="text" name="userName" onChange={updateCredentials} onBlur={()=>checkError("name")}/>
+                <label className="labelsLogin" for="name">NAME</label>
+                <input require="true" className="inputsLogin" type="text" name="name" onChange={updateCredentials} onBlur={()=>checkError("name")} placeholder="Name"/>
+                <div>{errors.eName}</div>
                 <label className="labelsLogin" for="nick">NICK</label>
-                <input require="true" className="inputsLogin" type="text" name="nick" onChange={updateCredentials} onBlur={()=>checkError("nick")}/>
+                <input require="true" className="inputsLogin" type="text" name="nick" onChange={updateCredentials} onBlur={()=>checkError("nick")} placeholder="Nick"/>
+                <div>{errors.eNick}</div>
                 <label className="labelsLogin" for="email">EMAIL</label>
-                <input require="true" className="inputsLogin" type="email" name="email" onChange={updateCredentials} onBlur={()=>checkError("email")}/>
+                <input require="true" className="inputsLogin" type="email" name="email" onChange={updateCredentials} onBlur={()=>checkError("email")} placeholder="Email"/>
+                <div>{errors.eEmail}</div>
                 <label className="labelsLogin" for="password">PASSWORD</label>
-                <input require="true" className="inputsLogin" type="password" name="password" onChange={updateCredentials} onBlur={()=>checkError("password")}/>
+                <input require="true" className="inputsLogin" type="password" name="password" onChange={updateCredentials} onBlur={()=>checkError("password")} placeholder="Password"/>
+                <div>{errors.ePassword}</div>
                 <label className="labelsLogin" for="birthdate">BIRTHDATE</label>
-                <input className="inputsLogin" type="date" name="birthdate" onChange={updateCredentials} onBlur={()=>checkError("")}/>
+                <input className="inputsLogin" type="date" name="birthdate" onChange={updateCredentials} onBlur={()=>checkError("birthdate")} placeholder="Birth date"/>
+                <div>{errors.eBirthdate}</div>
                 <label className="labelsLogin" for="country">COUNTRY</label>
-                <input className="inputsLogin" type="text" name="country" onChange={updateCredentials} onBlur={()=>checkError("name")}/>
+                <input className="inputsLogin" type="text" name="country" onChange={updateCredentials} onBlur={()=>checkError("country")} placeholder="Country"/>
+                <div>{errors.eCountry}</div>
                 <label className="labelsLogin" for="city">CITY</label>
-                <input className="inputsLogin" type="text" name="city" onChange={updateCredentials} onBlur={()=>checkError("name")}/>
+                <input className="inputsLogin" type="text" name="city" onChange={updateCredentials} onBlur={()=>checkError("city")} placeholder="City"/>
+                <div>{errors.eCity}</div>
     
                 
 
