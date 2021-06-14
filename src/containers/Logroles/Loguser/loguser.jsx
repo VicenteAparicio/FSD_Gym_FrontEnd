037f25a4467aeb2a9d3a03862b4009107e0a99b0
migueltmsp <migueltm.sp@gmail.com>
2021-06-14 02:23:29 +0200

    Created userLessons

--- src/containers/Logroles/Loguser/loguser.jsx
+++ src/containers/Logroles/Loguser/loguser.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useHistory} from 'react-router-dom';
+import axios from 'axios';
 import './loguser.css';
 
 const Loguser = () => {
@@ -24,18 +25,43 @@ const Loguser = () => {
         history.push("/login")
     }
 
+    const MyLessons = () => {
+        
+        history.push("/userlessons")
+
+        console.log("Id barrabaja",userData.user._id)
+        let body = {
+            userId : userData.user._id,
+        }
+
+        axios
+        .post('http://localhost:3005/user/all_my_lessons', body, {headers: {'Authorization': `Basic ${userData.token}`}})
+        .then((res)=>{
+            console.log(res.data[0].title)
+            console.log(res.data[1].title)
+            localStorage.setItem(res)
+        })
+        .catch((error)=>{
+            console.log("No entra a res");
+        });  
+    }
+
     if (userData.token){
         return (
             <div>
-                
-                {userData.user.nick}
-                {userData.user.name}
-                {userData.user.email}
-                {userData.user.birthdate}
-                {userData.user.city}
-                {userData.user.country}
+                <div className= "userInformation">
+                    {userData.user.nick}
+                    {userData.user.name}
+                    {userData.user.email}
+                    {userData.user.birthdate}
+                    {userData.user.city}
+                    {userData.user.country}
+                 </div>
+
+                <div className="myLessons" onClick={()=>MyLessons()}>My Lessons</div>
 
                 <div className="logOut" onClick={()=>Logout()}>LogOut!!</div>
+
             </div>
         )
     } else {
