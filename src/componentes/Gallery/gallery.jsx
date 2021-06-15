307150fd4f4e1b43c33fc2086c691c0f0c9dec40
Goodbyez <miradadesigual@gmail.com>
2021-06-15 14:06:12 +0200

    Working on a react gallery

--- src/componentes/Gallery/gallery.jsx
+++ src/componentes/Gallery/gallery.jsx
@@ -12,7 +12,7 @@ import './gallery.css';
 const Gallery = () => {
 
     // HOOKS
-    const [pic, setPic] = useState();
+    const [photo, setPhoto] = useState();
 
     // EQUIVALENTE AL COMPONENTDIDMOUNT en componentes de clase (sólo se ejecuta una vez)
     useEffect(()=>{
@@ -20,16 +20,42 @@ const Gallery = () => {
     },[]);
 
     // EQUIVALENTE AL COMPONENTDIDUPDATE en componentes de clase
-    useEffect(()=>{
-
+    useEffect((ev)=>{
+        setPhoto('');
+        console.log(ev);
+        setPhoto(ev);
     });
 
-    const showBig = () => {
 
+    const PulsaFoto = (pic) => {
+        setPhoto('');
+        console.log(pic);
+        setPhoto(pic);
     }
 
     return (
         <div id="gallery01" className="galleryContainer">
+
+				<div className="bigPicture">
+					<img className="img" src={photo} alt="Foto entrenamientos en Crosstraining"/>
+				</div>
+				<div className="picContainer">
+					<img alt="Foto entrenamientos en Crosstraining" title="Crosstraining" className="sPGallery01" id="img0" onClick={()=>PulsaFoto({Smallpic01})} src={Smallpic01}/>
+					<img alt="Foto entrenamientos en Crosstraining" title="Crosstraining" className="sPGallery01" id="img1" onClick={()=>PulsaFoto({Smallpic02})} src={Smallpic02}/>
+					<img alt="Foto entrenamientos en Crosstraining" title="Crosstraining" className="sPGallery01" id="img2" onClick={()=>PulsaFoto({Smallpic03})} src={Smallpic03}/>
+					<img alt="Foto entrenamientos en Crosstraining" title="Crosstraining" className="sPGallery01" id="img3" onClick={()=>PulsaFoto({Smallpic04})} src={Smallpic04}/>
+					<img alt="Foto entrenamientos en Crosstraining" title="Crosstraining" className="sPGallery01" id="img3" onClick={()=>PulsaFoto({Smallpic05})} src={Smallpic05}/>
+					<img alt="Foto entrenamientos en Crosstraining" title="Crosstraining" className="sPGallery01" id="img3" onClick={()=>PulsaFoto({Smallpic06})} src={Smallpic06}/>
+				</div>
+
+
+
+
+
+
+
+
+{/* 
             <div id="bigPic">
                 <img className="bigPicture" src={Smallpic01}/>
             </div>
@@ -43,7 +69,7 @@ const Gallery = () => {
                     <img alt="Foto entrenamientos en Crosstraining" title="Crosstraining" className="sPGallery01" src={Smallpic05} onClick={showBig(this)}/>
                     <img alt="Foto entrenamientos en Crosstraining" title="Crosstraining" className="sPGallery01" src={Smallpic06} onClick={showBig(this)}/>
                 </div>
-            </div>
+            </div> */}
 
         </div>
     )
