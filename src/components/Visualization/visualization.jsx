4359e3521f864aaf02698c22b82c34398be20207
Goodbyez <miradadesigual@gmail.com>
2021-06-17 16:52:10 +0200

    Developing visualization menu

---
+++ src/components/Visualization/visualization.jsx
@@ -0,0 +1,21 @@
+//IMPORT MOTORS
+import React from 'react';
+import {connect} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+// IMPORT ACTIONS
+import { DESTINY } from '../../redux/types';
+// IMPORT STYLE
+import './visualization.css'
+
+
+const Visualization = (props) => {
+
+    return (
+        <div className="vistaVisual bgGreen">
+            
+        </div>
+    )
+
+}
+
+export default connect()(Visualization);
