ccaaeda88ff883c0cfbc3452d2f7e106a7fa4c2f
migueltmsp <migueltm.sp@gmail.com>
2021-06-21 08:49:36 +0200

    Trying to fix git messes

--- src/containers/Logroles/Displaycoaches/displaycoaches.jsx
+++ src/containers/Logroles/Displaycoaches/displaycoaches.jsx
@@ -57,12 +57,12 @@ const DisplayAllCoaches = (props) => {
                     <Topjungle id="hide" title="DISPLAY COACHES"/>
                     <Titlesection title='OUR COACHES'/>
                     <div className="lessonsBox">
-                        {coachesInfo.map((lesson, index)=>(     
+                        {coachesInfo.map((coach, index)=>(     
                         <div className="lessonCard bgGreen txtWhite">
                             TEST TEXT
-                            <div className="lessonName norwester">NAME: {/* {lesson.title} */}</div> 
-                            <div className="lessonInfo dinC">TESTFUNCTION: {/* {lesson.coaches[0].name || "No hay ningún entrenador asociado"} */}</div>
-                            <div className="lessonInfo dinC">BLAHBLAH:{/*  {lesson.date} */}</div>
+                            <div className="lessonName norwester">Name: {{coach}}</div> 
+                            <div className="lessonInfo dinC">Task: {/* {lesson.coaches[0].name || "No hay ningún entrenador asociado"} */}</div>
+                            <div className="lessonInfo dinC">Valoration:{/*  {lesson.date} */}</div>
                             {/* <div className="lessonInfo dinC">{lesson.members.map((members, index)=>(
                                 <div className="lessonInfo dinC">Member: {members.nick ||  "No hay usuarios apuntados"}</div>))}
                             </div> */}
